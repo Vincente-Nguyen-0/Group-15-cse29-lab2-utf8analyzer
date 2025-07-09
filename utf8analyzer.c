5667a1cfe8f84717bf8358e84047c53d8be33cda
Yang <xiy070@ieng6-201.ucsd.edu>
2025-07-09 12:57:14 -0700

    Cindy 2

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,12 +4,24 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+int ascii_check(char str[]) {
+    int i = 0;
+    while(str[i] != '\0') {
+        if((unsigned char) str[i] > 127) return 0;
+        i++;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
         return 1;
-	//cindy
     }
+    char input[2048];
+    strncpy(input, argv[1], sizeof(input));
+    input[sizeof(input) - 1] = '\0';
 
-    // implement the UTF-8 analyzer here
+    int ascii = ascii_check(input);
+    printf("Valid ASCII: %s\n", ascii ? "true" : "false");
 }
